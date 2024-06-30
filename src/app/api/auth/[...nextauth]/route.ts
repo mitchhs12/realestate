23ec475d554a60d2a461f08e1b4504a8a88a7a68
valodax <snitzel098@gmail.com>
2024-06-30 18:33:40 -0500

    :white_check_mark: `quicksave` quicksaving site

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+// import NextAuth from "next-auth";
+// import Providers from "next-auth/providers";
 
-const handler = NextAuth({
-  ...
-})
+// const handler = NextAuth({});
 
-export { handler as GET, handler as POST }
+// export { handler as GET, handler as POST };
