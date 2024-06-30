bc27ff553f68768e54e3ae8201852f8d074ad301
valodax <snitzel098@gmail.com>
2024-06-30 18:41:47 -0500

    :white_check_mark: `quicksave` starting search bar

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Hero() {
   return (
     <div className="flex w-full h-[30vh]">
-      <div className="z-10 flex flex-col justify-center items-center h-full w-full bg-stone-200 dark:bg-stone-900">
+      <div className="z-10 flex gap-10 flex-col justify-center items-center h-full w-full bg-stone-200 dark:bg-stone-900">
         <h1 className="text-3xl font-bold">Welcome to Propertuna!</h1>
-        <p className="text-xl mt-4">(Search box goes here)</p>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="search" placeholder="Search for properties" />
+          <Button type="submit">Search</Button>
+        </div>
       </div>
     </div>
   );
