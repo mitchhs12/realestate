50a4efaedeec76ce361d9c29258318e700be73b4
valodax <snitzel098@gmail.com>
2024-06-30 18:56:24 -0500

    :hammer: `fix` fixed the auth bug

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,11 +4,19 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <div className="flex w-full h-[30vh]">
-      <div className="z-10 flex gap-10 flex-col justify-center items-center h-full w-full bg-stone-200 dark:bg-stone-900">
+    <div className="relative flex w-full h-[30vh]">
+      <Image
+        src="/house.jpg"
+        alt="background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="-z-10 opacity-30"
+      />
+      <div className="z-10 flex space-y-10 flex-col justify-center items-center h-full w-full bg-primary-background dark:bg-primary-background">
         <h1 className="text-3xl font-bold">Welcome to Propertuna!</h1>
         <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input type="search" placeholder="Search for properties" />
+          <Input type="search" placeholder="Search locations" />
           <Button type="submit">Search</Button>
         </div>
       </div>
