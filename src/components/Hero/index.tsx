070b0208ea216fbe1b986e539bc4317a25a9644d
valodax <snitzel098@gmail.com>
2024-07-01 18:39:34 -0500

    :white_check_mark: `quicksave` quicksaving for auth

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
         className="-z-10 opacity-30"
       />
       <div className="z-10 flex space-y-10 flex-col justify-center items-center h-full w-full">
-        <h1 className="text-3xl font-bold">Welcome to Propertuna!</h1>
+        <h1 className="text-3xl font-bold">Welcome to Propertuna!!</h1>
         <div className="flex w-full max-w-sm items-center space-x-2">
           <Input type="search" placeholder="Search locations" className="bg-muted" />
           <Button type="submit">Search</Button>
