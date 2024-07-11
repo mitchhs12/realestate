b6e9815093dc0701a1a999958e2458551885d3cc
valodax <snitzel098@gmail.com>
2024-07-11 15:12:55 -0500

    :white_check_mark: `quicksave` fix

--- src/components/CombinedSearchPage/index.tsx
+++ src/components/CombinedSearchPage/index.tsx
@@ -18,18 +18,10 @@ export default function CombinedSearchPage({ coordinates }: { coordinates: Coord
 
   return (
     <>
-      <section
-        className={`flex w-full h-1/2 ${
-          !mapFocused ? "md:hidden" : "md:h-full"
-        } lg:flex lg:w-1/2 lg:h-full border border-red-400 flex-grow`}
-      >
+      <section className={`flex w-full h-1/2 ${!mapFocused ? "md:hidden" : "md:h-full"} lg:flex lg:w-1/2 lg:h-full`}>
         <SearchResults coordinates={coordinates} />
       </section>
-      <section
-        className={`flex w-full h-1/2 ${
-          mapFocused ? "md:hidden" : "md:h-full"
-        } lg:flex lg:w-1/2 lg:h-screen-minus-header border border-red-400`}
-      >
+      <section className={`flex w-full h-1/2 ${mapFocused ? "md:hidden" : "md:h-full"} lg:flex lg:w-1/2 lg:h-full`}>
         <MapComponent coordinates={coordinates} />
       </section>
       <FloatingButton />
