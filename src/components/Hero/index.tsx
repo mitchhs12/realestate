98da17baf3650251e310cb6cb696ce74a97168df
valodax <snitzel098@gmail.com>
2024-07-13 16:40:35 -0500

    :hammer: `fix` fixed the zoom in on click and suggestions appearing above the search box on homepage test

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,7 +4,7 @@ import { poppins, montserrat } from "@/app/fonts";
 
 export default function Hero() {
   return (
-    <div className="relative h-[30vh] flex w-full">
+    <div className="relative h-[20vh] flex w-full">
       <Image
         src="/house.jpg"
         alt="background"
@@ -14,12 +14,14 @@ export default function Hero() {
         priority={true}
         className="-z-10 opacity-30 dark:opacity-20"
       />
-      <div className="flex flex-col w-full justify-center items-center gap-y-10">
-        <div className="z-10 flex flex-col w-full justify-center items-center space-y-10">
-          <h1 className={`${poppins.className} text-center text-2xl md:text-3xl lg:text-3xl font-light tracking-wider`}>
+      <div className="absolute inset-0 flex flex-col w-full justify-center items-center">
+        <div className="flex flex-col gap-y-4">
+          <h1
+            className={`${poppins.className} flex text-center justify-center text-xl md:text-3xl font-light tracking-wider`}
+          >
             Find your dream home.
           </h1>
-          <div className="flex justify-center items-center w-full">
+          <div className="flex justify-center items-center">
             <SearchBox isSmallMap={false} />
           </div>
         </div>
