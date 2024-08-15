0cf0f182e74690c4ccd97dae0a224abc11cf6279
valodax <snitzel098@gmail.com>
2024-08-14 19:47:41 -0500

    :white_check_mark: `quicksave` quicksave

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["vivaidealfinalbucket.s3.us-west-2.amazonaws.com"],
+    remotePatterns: [{ protocol: "https", hostname: "vivaidealfinalbucket.s3.us-west-2.amazonaws.com", port: "" }],
   },
 };
 
