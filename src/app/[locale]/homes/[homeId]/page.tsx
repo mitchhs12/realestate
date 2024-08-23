e58cea7c3e183d21da9837c37e34701c24abdc44
valodax <snitzel098@gmail.com>
2024-08-23 01:13:18 -0500

    :white_check_mark: `quicksave` quicksave

--- src/app/[locale]/homes/[homeId]/page.tsx
+++ src/app/[locale]/homes/[homeId]/page.tsx
@@ -22,8 +22,8 @@ export default async function Page({ params }: { params: { homeId: string } }) {
 
   if (home) {
     return (
-      <main className="flex flex-col min-h-screen-minus-header-svh md:flex-row justify-center">
-        <div className="flex flex-col border-2 w-full max-w-7xl pt-4 gap-4 border-green-500 p-8">
+      <main className="flex flex-col md:justify-center min-h-screen-minus-header-svh md:flex-row">
+        <div className="flex flex-col text-center border-2 h-full max-w-7xl w-full pt-8">
           <h1 className="flex justify-center text-3xl">{home.title}</h1>
           <HomePhotos home={home} />
         </div>
--- src/components/HomePhotos/index.tsx
+++ src/components/HomePhotos/index.tsx
@@ -1,21 +1,48 @@
 import Image from "next/image";
 import { HomeType } from "@/lib/validations";
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
 
 export default function HomePhotos({ home }: { home: HomeType }) {
   return (
-    <div className="flex justify-center h-full border-2 border-blue-500">
-      <div className="grid grid-cols-4 grid-rows-2 gap-4 h-1/2 w-full">
-        {home.photos.slice(0, 5).map((photo, index) => (
-          <div key={index} className={`relative ${index === 0 ? "row-span-2 col-span-2 h-full w-full" : ""}`}>
-            <Image
-              src={photo}
-              className="object-cover object-center rounded-lg"
-              alt={`${home.title} photo ${index}`}
-              fill={true}
-              sizes={"(max-width: 500px), (max-height: 500px)"}
-            />
-          </div>
-        ))}
+    <div className="flex flex-col min-h-full w-full p-8">
+      <div className="flex justify-center min-h-full w-full">
+        {/* Grid for larger screens */}
+        <div className="hidden md:grid grid-cols-4 grid-rows-2 gap-4 h-80 lg:h-96 w-full">
+          {home.photos.slice(0, 5).map((photo, index) => (
+            <div key={index} className={`relative ${index === 0 ? "row-span-2 col-span-2" : ""} h-full w-full`}>
+              <Image
+                src={photo}
+                className="object-cover object-center rounded-lg"
+                alt={`${home.title} photo ${index}`}
+                fill
+                sizes="(max-width: 500px), (max-height: 500px)"
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Carousel for smaller screens */}
+        <div className="md:hidden w-full">
+          <Carousel className="relative h-full w-full">
+            <CarouselContent>
+              {home.photos.map((photo: string, index) => (
+                <CarouselItem key={index} className="flex justify-center items-center h-full w-full">
+                  <div className="relative justify-center items-center h-48 xs:h-64 sm:h-72 w-full">
+                    <Image
+                      src={photo}
+                      className="object-cover object-center rounded-lg"
+                      alt={`${home.title} photo ${index}`}
+                      fill
+                      sizes="(max-width: 500px) 100vw, (max-height: 500px) 100vh"
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="hidden sm:flex absolute left-6 size-8" />
+            <CarouselNext className="hidden sm:flex absolute right-6 size-8" />
+          </Carousel>
+        </div>
       </div>
     </div>
   );
