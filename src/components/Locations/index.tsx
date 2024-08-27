81270c3e809ff98ba1cf81d78b97b400e8e41152
valodax <snitzel098@gmail.com>
2024-08-27 14:02:26 -0500

    :white_check_mark: `quicksave` quicksaving images

--- src/components/Locations/index.tsx
+++ src/components/Locations/index.tsx
@@ -150,7 +150,7 @@ export default function Locations() {
   function generateUrl(placeName: string, countryName: string) {
     const baseUrl = "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home";
     const formattedPlace = placeName.toLowerCase().replace(/, /g, "/").replace(/ /g, "+").replace(/\./g, "");
-    return `${baseUrl}/${countryName.toLowerCase().trim()}/${formattedPlace}.avif`;
+    return `${baseUrl}/${countryName.toLowerCase().trim()}/${formattedPlace}.webp`;
   }
 
   const urlMap = imageMap.reduce(
