f454aa93fbbd549ec0f19efe0de5f83326692168
valodax <snitzel098@gmail.com>
2024-09-01 21:40:50 -0500

    :white_check_mark: `quicksave` updated middleware matcher function

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
+  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt|studio).*)"],
 };
