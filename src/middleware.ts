54e506cf7fde7241424b8c87e610190386386dfd
valodax <snitzel098@gmail.com>
2024-09-01 22:26:42 -0500

    :white_check

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,5 +17,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt|studio).*)"],
+  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt|studio|auth|login|callback|logout).*)"],
 };
