ae41e16d044b49762d43d0290944364e59ea81d3
valodax <snitzel098@gmail.com>
2024-09-12 22:42:58 -0500

    :white_check_mark: `quicksave` cloudfront

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,9 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        domains: "d3pkwgsrr79pi4.cloudfront.net",
-        hostname: "vivaidealfinalbucket.s3.us-west-2.amazonaws.com",
-        port: "",
-      },
+      { protocol: "https", hostname: "vivaidealfinalbucket.s3.us-west-2.amazonaws.com", port: "" },
       { protocol: "https", hostname: "cdn.sanity.io", port: "" },
+      { protocol: "https", hostname: "d3pkwgsrr79pi4.cloudfront.net", port: "" },
     ],
   },
 };
