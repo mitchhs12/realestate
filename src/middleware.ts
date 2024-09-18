f182d5cc1e6a0f26509a615feb77963def626deb
valodax <snitzel098@gmail.com>
2024-09-18 13:38:49 -0500

    :white_check_mark: `quicksave` header test

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,25 +10,25 @@ const I18nMiddleware = createI18nMiddleware({
 
 export function middleware(request: NextRequest) {
   // Get the "Accept-Language" header from the request
-  // const acceptLanguage = request.headers.get("Accept-Language");
+  const acceptLanguage = request.headers.get("Accept-Language");
 
-  // // Detect the preferred language from the Accept-Language header
-  // let detectedLocale = defaultLanguage;
-  // let matchedCurrency = "USD";
+  // Detect the preferred language from the Accept-Language header
+  let detectedLocale = defaultLanguage;
+  let matchedCurrency = "USD";
 
-  // if (acceptLanguage) {
-  //   const language = acceptLanguage.split(",")[0]; // Primary language
-  //   // Match the detected language with your supported locales
-  //   detectedLocale = languages.includes(language as LanguageType) ? language : defaultLanguage;
+  if (acceptLanguage) {
+    const language = acceptLanguage.split(",")[0]; // Primary language
+    // Match the detected language with your supported locales
+    detectedLocale = languages.includes(language as LanguageType) ? language : defaultLanguage;
 
-  //   // Get the matched currency for the detected locale
-  //   matchedCurrency = locales.find((option) => option.locale === detectedLocale)?.currency || "USD";
-  // }
+    // Get the matched currency for the detected locale
+    matchedCurrency = locales.find((option) => option.locale === detectedLocale)?.currency || "USD";
+  }
 
   // Use custom headers to pass the locale and currency to the server-side
   const response = I18nMiddleware(request);
-  // response.headers.set("x-locale", detectedLocale);
-  // response.headers.set("x-currency", matchedCurrency);
+  response.headers.set("x-locale", detectedLocale);
+  response.headers.set("x-currency", matchedCurrency);
 
   return response;
 }
