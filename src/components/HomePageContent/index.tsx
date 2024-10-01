1e3242c7c41dede6776c02bfe4e03b3aa7e606b8
valodax <snitzel098@gmail.com>
2024-10-01 17:37:23 -0500

    :rocket: `feat` gradient homepage

--- src/components/HomePageContent/index.tsx
+++ src/components/HomePageContent/index.tsx
@@ -92,7 +92,7 @@ export default async function HomePageContent() {
 
   return (
     <div className="flex flex-col justify-start h-full w-full sm:gap-2 bg-background">
-      <section className="flex bg-gradient-to-r from-[#e8f6ed] to-white dark:from-[#04210f] dark:to-black flex-col justify-center items-center w-full h-full">
+      <section className="flex bg-gradient-to-b from-[#f4fbf6] to-[#f4f5fb] dark:from-[#100e02] dark:to-[#021003] flex-col justify-center items-center w-full h-full">
         <div className="flex flex-col pt-4 pb-1 md:pb-4 justify-start w-full h-full max-w-8xl">
           <h2 className="flex justify-center items-center gap-3 text-3xl sm:text-4xl py-4 font-normal">
             <Map size={32} strokeWidth={2} />
@@ -101,7 +101,7 @@ export default async function HomePageContent() {
           <Locations countries={countries} />
         </div>
       </section>
-      <section className="flex flex-col bg-[#e8eaf6] dark:bg-[#040821] justify-center items-center w-full h-full">
+      <section className="flex flex-col bg-gradient-to-b from-[#f4f5fb] to-[#fbf4f8] dark:from-[#021007] dark:to-[#020410] justify-center items-center w-full h-full">
         <div className="flex flex-col pt-4 pb-1 md:pb-4 justify-start h-full w-full max-w-8xl">
           <h2 className="flex justify-center items-center gap-3 text-3xl sm:text-4xl py-4 font-normal">
             <Heart size={32} strokeWidth={2.4} />
@@ -110,7 +110,7 @@ export default async function HomePageContent() {
           <Listings listingKey={"popular"} typesObject={typesObject} loginToViewPrice={loginToViewPrice} />
         </div>
       </section>
-      <section className="flex flex-col bg-[#f6e8f1] dark:bg-[#210416] justify-center items-center w-full h-full">
+      <section className="flex flex-col bg-gradient-to-b from-[#fbf4f8] to-[#fbfaf4] dark:from-[#020410] dark:to-[#10020b] justify-center items-center w-full h-full">
         <div className="flex flex-col pt-4 pb-1 md:pb-4 justify-start h-full w-full max-w-8xl">
           <h2 className="flex justify-center items-center gap-3 text-3xl sm:text-4xl py-4 font-normal">
             <HandCoins size={32} strokeWidth={2} />
@@ -119,7 +119,7 @@ export default async function HomePageContent() {
           <Listings listingKey={"cheap"} typesObject={typesObject} loginToViewPrice={loginToViewPrice} />
         </div>
       </section>
-      <section className="flex flex-col bg-[#f6f4e8] dark:bg-[#211d04] justify-center items-center w-full h-full">
+      <section className="flex flex-col bg-gradient-to-b from-[#fbfaf4] to-[#f4fbf6] dark:from-[#10020b] dark:to-[#100e02] justify-center items-center w-full h-full">
         <div className="flex flex-col pt-4 pb-1 md:pb-4 justify-start h-full w-full max-w-8xl">
           <h2 className="flex justify-center items-center gap-3 text-3xl sm:text-4xl py-4 font-normal">
             <Newspaper size={32} strokeWidth={2} />
