45a0194d07d74e8f8d0176e5e51091f3f5cb2175
valodax <snitzel098@gmail.com>
2024-10-05 21:20:04 -0500

    :rocket: `feat` apple

--- src/auth.ts
+++ src/auth.ts
@@ -34,12 +34,6 @@ const providers: Provider[] = [
         name: profile.name || null, //profile.name.givenName + " " + profile.name.familyName, but apple does not return name...
         email: profile.email || null,
         image: null,
-        // phoneNumber: null,
-        // currency: null,
-        // language: null,
-        // role: null,
-        // favoritedLists: [],
-        // homes: [],
       };
     },
   }),
