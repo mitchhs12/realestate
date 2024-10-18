5587e3fbb5640abb25a9c20c3974d258b9b44953
valodax <snitzel098@gmail.com>
2024-10-17 22:37:51 -0500

    :rocket: `feat` linkedin

--- src/components/Footer/Icons/index.tsx
+++ src/components/Footer/Icons/index.tsx
@@ -4,35 +4,42 @@ import { Button } from "@/components/ui/button";
 export default function Icons() {
   return (
     <div className="flex gap-5 justify-center">
-      <Button variant="outline" size={"lgSqIcon"} className="rounded-xl">
+      <Button variant="secondary" size={"lgSqIcon"} className="rounded-xl">
         <SocialIcon
           url="https://x.com/viva_ideal"
           className="bg-[#000000] rounded-lg"
           style={{ height: 40, width: 40 }}
         />
       </Button>
-      <Button variant="outline" size={"lgSqIcon"} className="rounded-xl">
+      <Button variant="secondary" size={"lgSqIcon"} className="rounded-xl">
         <SocialIcon
           url="https://www.facebook.com/profile.php?id=61564984446949"
           className="bg-[#3B5998] rounded-lg"
           style={{ height: 40, width: 40 }}
         />
       </Button>
-      <Button variant="outline" size={"lgSqIcon"} className="rounded-xl">
+      <Button variant="secondary" size={"lgSqIcon"} className="rounded-xl">
         <SocialIcon
           url="https://www.instagram.com/viva.ideal"
           className="bg-[#E94475] rounded-lg"
           style={{ height: 40, width: 40 }}
         />
       </Button>
-      <Button variant="outline" size={"lgSqIcon"} className="rounded-xl">
+      <Button variant="secondary" size={"lgSqIcon"} className="rounded-xl">
+        <SocialIcon
+          url="https://www.linkedin.com/company/viva-ideal/"
+          className="bg-[#007FB1] rounded-lg"
+          style={{ height: 40, width: 40 }}
+        />
+      </Button>
+      <Button variant="secondary" size={"lgSqIcon"} className="rounded-xl">
         <SocialIcon
           url="https://www.tiktok.com/@vivaideal"
           className="bg-[#000000] rounded-lg"
           style={{ height: 40, width: 40 }}
         />
       </Button>
-      <Button variant="outline" size={"lgSqIcon"} className="rounded-xl">
+      <Button variant="secondary" size={"lgSqIcon"} className="rounded-xl">
         <SocialIcon
           network={"whatsapp"}
           className="bg-[#25D366] rounded-lg"
