6ca03ca1bf312e27b2f24192c3c8aa851862419d
valodax <snitzel098@gmail.com>
2024-11-12 18:00:43 -0500

    :white_check_mark: `quicksave` quicksave

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -5,13 +5,14 @@ export default function Clients() {
   const images = [
     "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/construvalores.avif",
     "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/ibra-gomez.png",
-    "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/ecuaraices.png",
     "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/coritrec.png",
+    "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/ecuaraices.png",
     "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/inmoga.png",
+    "https://vivaidealfinalbucket.s3.us-west-2.amazonaws.com/home/clients/ehf.png",
   ];
 
   return (
-    <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-12">
+    <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-y-12">
       {images.map((image, index) => (
         <div key={index} className="relative justify-center items-center w-auto h-[140px]">
           <Image
