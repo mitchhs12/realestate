52c998443cdfb68335c97b2a75c3fe30e067a137
valodax <snitzel098@gmail.com>
2024-11-23 15:51:03 -0500

    :hammer: `fix` edit image fix

--- src/app/[locale]/homes/[homeId]/edit/photos/index.tsx
+++ src/app/[locale]/homes/[homeId]/edit/photos/index.tsx
@@ -279,7 +279,7 @@ export default function Photos() {
   return (
     <div className="flex flex-col h-full w-full items-center gap-y-10">
       <div className="flex flex-col w-full h-full justify-start items-center text-center gap-y-5">
-        <div className="flex flex-col pb-4">
+        <div className="flex flex-col">
           <div className="flex items-center justify-center py-3">
             <h1 className="flex items-center text-3xl">{t("title")}</h1>
           </div>
@@ -295,12 +295,12 @@ export default function Photos() {
             {uploadedImageUrls.length < 5 && <h4>{t("restriction")}</h4>}
           </div>
         </div>
-        <div className="flex flex-col px-8 gap-4 w-full lg:h-full justify-center items-center h-full overflow-auto">
+        <div className="flex flex-col px-8 gap-4 w-full lg:h-full justify-start items-center h-full overflow-auto">
           <div className="flex justify-center text-center text-red-500 text-xs md:text-base lg:text-lg">
             {errorMessage}
           </div>
           <div className={`flex flex-col w-full h-full max-w-7xl items-center justify-start gap-3`}>
-            <div className="grid grid-cols-2 sm:grid-cols-3 sm:grid-rows-4 lg:grid-cols-4 lg:grid-rows-3 gap-4 pb-8 w-full">
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 pb-8 w-full">
               <DndContext
                 sensors={sensors}
                 collisionDetection={closestCenter}
