6b74d2114365fe8dbb16bd48477aed2793834768
valodax <snitzel098@gmail.com>
2024-11-28 23:13:35 -0500

    :white_check_mark: `quicksave` quicksave

---
+++ src/app/[locale]/search/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/Footer";
+
+export default async function Searchlayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="flex flex-col items-center w-full">
+        <main className="flex-grow w-full h-full min-h-screen-minus-header-svh">{children}</main>
+        <footer className="flex justify-center items-center p-6 w-full bg-muted">
+          <Footer />
+        </footer>
+      </div>
+    </>
+  );
+}
