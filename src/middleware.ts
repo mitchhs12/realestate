83ae99eb25a9996eb4d6f7a9ed3f14742de49917
valodax <snitzel098@gmail.com>
2024-12-13 18:08:44 -0500

    :white_check_mark: `quciksave` middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { languages, defaultLanguage } from "@/lib/validations";
 import { createI18nMiddleware } from "next-international/middleware";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { LanguageType } from "@/lib/validations";
 
 // Create the I18n middleware
@@ -28,7 +28,14 @@ const I18nMiddleware = createI18nMiddleware({
 // Combined middleware function
 export async function middleware(request: NextRequest) {
   // Internationalization logic
+  const url = new URL(request.url);
+  const pathSegments = url.pathname.split("/").filter(Boolean);
 
+  // If someone visits `/studio` without locale, rewrite to `/${defaultLanguage}/studio`
+  if (pathSegments[0] === "studio") {
+    url.pathname = `/${defaultLanguage}/studio/${pathSegments.slice(1).join("/")}`;
+    return NextResponse.rewrite(url);
+  }
   const response = I18nMiddleware(request);
   return response;
 }
